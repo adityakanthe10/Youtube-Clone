ccac74e3cb183b9a677759e72299cf8244b46a90
Aditya Kanthe <adityakanthe10@gmail.com>
2024-04-11 19:07:38 +0530

    Created navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import { Routes,Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+        </Routes>
+      </div>
+  )
+}
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+
+const   = () => {
+  return (
+    <> 
+    <Sidebar/>
+    </>
+  )
+}
+
+export default  
